d248969894b64f972eed8a992fbc239a47856073
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2018-03-22 08:52:21 -0400

    #46601 QA Fix for case insensitive storage creation

--- python/setup_project/create_storage_dialog.py
+++ python/setup_project/create_storage_dialog.py
@@ -75,6 +75,21 @@ class CreateStorageDialog(QtGui.QDialog):
             return self._set_valid(
                 False, "* Storage name already exists.")
 
+        # case insensitive check against existing storage names. we do this
+        # separately from the case sensitive check in order to provide the user
+        # with more info and prevent confusion when they go looking in SG for a
+        # Foobar storage when it's foobar that's the one that exists. FYI, SG
+        # itself does not allow both Foobar and foobar storages.
+        lc_existing_storage_names = [
+            s.lower() for s in self._existing_storage_names]
+        if storage_name.lower() in lc_existing_storage_names:
+            index = lc_existing_storage_names.index(storage_name.lower())
+            return self._set_valid(
+                False,
+                "* Name is invalid. '%s' exists" %
+                (self._existing_storage_names[index])
+            )
+
         if not self.STORAGE_NAME_REGEX.match(storage_name):
             # storage name is invalid
             return self._set_valid(
